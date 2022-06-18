f980bd5c697a740759c1e513a21d10d4a5365bd4
Ezekiel Newren <ezekielnewren@gmail.com>
2022-06-18 09:20:29 -0600

    don't create a keyring for the root user for now

--- roles/keyring/files/keyring.sh
+++ roles/keyring/files/keyring.sh
@@ -3,8 +3,8 @@
 password=$(clevis decrypt tpm2 '{}' < ~/keyring.jwe)
 
 ## start the keyring for the root user
-dbus-run-session -- bash root-keyring.sh &
-disown -h
+#dbus-run-session -- bash root-keyring.sh &
+#disown -h
 
 for user in $(ls -1 /home); do
     machinectl shell $user@.host /usr/bin/bash -c "echo -n $password | gnome-keyring-daemon --daemonize --login"
