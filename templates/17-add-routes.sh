666c04919aab0de9dfe9a736220a6d57134e0e8a
Ezekiel Newren <ezekielnewren@gmail.com>
2024-06-11 03:53:09 -0600

    add the default route as part of bringing up the interface

--- templates/17-add-routes.sh
+++ templates/17-add-routes.sh
@@ -4,6 +4,7 @@ INTERFACE=$1
 STATUS=$2
 
 if [[ "$INTERFACE" == "br-fast" && "$STATUS" == "up" ]]; then
+    ip route add default via 192.168.17.1 dev br-fast table fast
     ip route add 192.168.17.0/24 dev br-fast scope link src {{ FAST_IP }} table fast
     ip rule add from all to 192.168.17.0/24 lookup fast
     ip rule add from 192.168.17.0/24 lookup fast
