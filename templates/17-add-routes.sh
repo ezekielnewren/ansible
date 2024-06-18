a2b94d524963f88409ae2832cc9c16a6608eea39
Ezekiel Newren <ezekielnewren@gmail.com>
2024-06-17 18:58:51 -0600

    add addtional rules to allow direct connections to 192.168.17.X from 192.168.16.X

--- templates/17-add-routes.sh
+++ templates/17-add-routes.sh
@@ -6,7 +6,9 @@ STATUS=$2
 if [[ "$INTERFACE" == "br-fast" && "$STATUS" == "up" ]]; then
     ip route add default via 192.168.17.1 dev br-fast table fast
     ip route add 192.168.17.0/24 dev br-fast scope link src {{ FAST_IP }} table fast
-    ip rule add from all to 192.168.17.0/24 lookup fast
-    ip rule add from 192.168.17.0/24 lookup fast
+    ip rule add from all to 192.168.17.0/24 lookup fast priority 200
+    ip rule add from 192.168.17.0/24 lookup fast priority 200
+    ip rule add not from 192.168.17.0/24 to {{ FAST_IP }} lookup main priority 100
+    ip rule add from {{ FAST_IP }} not to 192.168.17.0/24 lookup main priority 100
 fi
 
