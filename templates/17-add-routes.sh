891fa569d30352437d116d8af765bd4aa2be79bb
Ezekiel Newren <ezekielnewren@gmail.com>
2024-10-22 19:05:43 -0600

    don't treat the self fast address any different when routing traffic

--- templates/17-add-routes.sh
+++ templates/17-add-routes.sh
@@ -8,7 +8,10 @@ if [[ "$INTERFACE" == "br-fast" && "$STATUS" == "up" ]]; then
     ip route add 192.168.17.0/24 dev br-fast scope link src {{ FAST_IP }} table fast
     ip rule add from all to 192.168.17.0/24 lookup fast priority 200
     ip rule add from 192.168.17.0/24 lookup fast priority 200
-    ip rule add not from 192.168.17.0/24 to {{ FAST_IP }} lookup main priority 100
-    ip rule add from {{ FAST_IP }} not to 192.168.17.0/24 lookup main priority 100
+elif [[ "$INTERFACE" == "br-fast" && "$STATUS" == "down" ]]; then
+    ip route del default via 192.168.17.1 dev br-fast table fast
+    ip route del 192.168.17.0/24 dev br-fast scope link src {{ FAST_IP }} table fast
+    ip rule del from all to 192.168.17.0/24 lookup fast priority 200
+    ip rule del from 192.168.17.0/24 lookup fast priority 200
 fi
 
